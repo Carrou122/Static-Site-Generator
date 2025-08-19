ecad67230c1021739310c8906db68177edcdd523
Carrou122 <sufiadzhar122@gmail.com>
2025-08-19 21:35:56 +0800

    H:added regex extract links

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,8 @@
 import unittest
 
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -55,6 +56,61 @@ class TestDelimiter(unittest.TestCase):
         split_nodes_delimiter(nodes, "`", TextType.CODE),
         [TextNode("This is code and more code", TextType.TEXT)]
         )
+
+class TestMarkdown(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+    
+    def test_extract_multiple_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is a test with multiple images ![image](https://i.imgur.com/Df1x7tq.jpeg) and ![imag2](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/Df1x7tq.jpeg"), ("imag2", "https://i.imgur.com/zjjcJKZ.png")], matches)
+    
+    def test_with_no_image(self):
+        matches = extract_markdown_images(
+            "This is a test"
+        )
+        self.assertListEqual([], matches)
+
+    def test_with_both_image_n_links(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and link [to boot dev](https://www.boot.dev)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_with_empty_alt_text(self):
+        matches = extract_markdown_images(
+            "This is text with an ![](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev")], matches)
+
+    def test_extract_multiple_links(self):
+        matches = extract_markdown_links(
+            "This is text with multiple links [to boot dev](https://www.boot.dev) and [to google](https://www.google.com)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to google", "https://www.google.com")], matches)
+
+    def test_with_no_links(self):
+        matches = extract_markdown_links(
+            "This is text with no links"
+        )
+        self.assertListEqual([], matches)
+
+    def test_extract_markdown_links_with_images(self):
+        matches = extract_markdown_links(
+            "This is a test with link [to boot dev](https://www.boot.dev) and ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev")], matches)
 
 if __name__ == "__main__":
     unittest.main()
